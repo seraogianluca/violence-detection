ff95ef447cc350e479de15ad98152f5109eecb13
seraogianluca <g.serao@studenti.unipi.it>
2021-12-13 11:11:36 +0100

    working training

--- src/data/video.py
+++ src/data/video.py
@@ -1,15 +1,12 @@
 import os
-from numpy.random.mtrand import sample
 import torch
 import numpy as np
 
 from torch import Tensor
 from typing import Callable, Optional, Tuple
-from torch._C import dtype
 from torchvision.io import read_video
 from torchvision.datasets.folder import make_dataset
 from torchvision.datasets import VisionDataset
-from torch.utils.data import DataLoader
 
 class VideoRecord:
     def __init__(
@@ -80,17 +77,17 @@ class VideoDataset(VisionDataset):
         # useful to reuse transformations on images
         # batch of images -> Tensor[B, C, H, W]
         # B batch size, C channels
-        frames = torch.permute(frames, (0, 3, 1, 2)).type(torch.float32)
+        frames = torch.permute(frames, (0, 3, 1, 2))
 
         # cast uint8 to float32
         # TODO: check carefully how to do this cast
-        # torchvision.transforms.ToTensor
+        # convert range from [0,255] to [0,1] 
+        frames = frames.type(torch.float32).div(255)
 
         if self.transforms is not None:
             # consider the entire video as batch of images
             frames = self.transforms(frames)
-        
-        #return path_to_video, self.current_video, video_cls
+
         return frames, video_cls
 
 class SpatioTemporalDataset(VideoDataset):
@@ -99,11 +96,18 @@ class SpatioTemporalDataset(VideoDataset):
         root: str,
         transforms: Optional[Callable] = None,
         extensions: Tuple[str, ...] = ('.avi', '.mp4'),
-        num_clips: int = 16
-        ) -> None:
+        num_clips: int = 16,
+        train: bool = True) -> None:
 
         super().__init__(root, transforms, extensions)
         self.num_clips = num_clips
+        self.train = train
+    
+    def set_transforms(self, transforms):
+        self.transforms = transforms
+    
+    def set_train(self, train):
+        self.train = train
 
     def _sample_frames(
         self, 
@@ -121,16 +125,38 @@ class SpatioTemporalDataset(VideoDataset):
         offsets = np.multiply(list(range(self.num_clips)), average_duration) + np.random.randint(average_duration, size=self.num_clips)
 
         return torch.index_select(video, 0, torch.tensor(offsets, dtype=torch.int32))
+    
+    def _validation_frames(
+        self, 
+        video: Tensor) -> Tensor:
+        # divide a video into num_clip buckets, uniform sampling from each bucket.
+        # sort of segment based sampling
+        # video tensor is TxCxHxW
+        # TODO: sampling for validation
+        num_frames = video.shape[0]
+
+        if self.num_clips > num_frames:
+            raise ValueError('num_clips should be lower than frames in video')
+        
+        average_duration = video.shape[0] // self.num_clips
+        offsets = np.multiply(list(range(self.num_clips)), average_duration)
+
+        return torch.index_select(video, 0, torch.tensor(offsets, dtype=torch.int32))
 
     def __getitem__(
         self, 
         index: int) -> Tuple[Tensor, int]:
         video, label = super().__getitem__(index)
-        sampled_video = self._sample_frames(video)
 
+        if self.train:
+            sampled_video = self._sample_frames(video)
+        else:
+            sampled_video = self._validation_frames(video)
+        
         return sampled_video, label
 
 if __name__ == '__main__':
+    pass
     # import torchvision.transforms as T
     # import utils as U
  
@@ -161,17 +187,3 @@ if __name__ == '__main__':
     # print(f'Video shape: {video.shape}')
     # print(f'Video dtype: {video.dtype}')
     # print(f'Video class: {cls}')
-    
-    from tqdm import tqdm
-
-    def dataset_index(dataset):
-        dataset_itr = iter(dataset)
-
-        with open('./train_index.txt', 'w') as f:
-            for path, video, label in tqdm(dataset_itr):
-                #print(f'Path {path[0]}, Video {video.shape[1]}, Label {label.item()}')
-                f.write(path[0] + ' ' + str(video.shape[1]) + ' ' + str(label.item()) + '\n')
-    
-    video_dataset = VideoDataset('/mnt/d/serao/fight_detection')
-    dataset = DataLoader(video_dataset, num_workers=0)
-    dataset_index(dataset)
--- src/train_test.py
+++ src/train_test.py
@@ -1,15 +1,15 @@
 import torch
+from torch.nn.functional import threshold
+from torch.optim import lr_scheduler
 import torchvision
 import torchmetrics
 import pytorch_lightning as pl
 import torch.nn as nn
-import torch.nn.functional as F
 
 from typing import Optional
 
-from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
 from torch.utils.data.dataloader import DataLoader
-from torchvision.transforms.transforms import ToPILImage
 from data.video import SpatioTemporalDataset
 from data.utils import stratified_random_split
 from torchvision.models.video import r3d_18
@@ -18,61 +18,98 @@ class ViolenceDetection(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.model = r3d_18(pretrained=True)
-        self.model.fc = nn.Linear(512, 1, bias=True)
-
-        self.val_accuracy = torchmetrics.Accuracy()
+        self.model.fc = nn.Linear(512, 1)
+        self.criterion = nn.BCELoss()
+        self.train_accuracy = torchmetrics.Accuracy()
+        self.valid_accuracy = torchmetrics.Accuracy()
     
     def forward(self, x):
-        return torch.sigmoid(self.model(torch.permute(x, (0, 2, 1, 3, 4))))
+        x = torch.permute(x, (0,2,1,3,4))
+        out = self.model(x)
+        return out
     
     def training_step(self, batch, batch_idx):
         # B x T x C x H x W
         x, y = batch
         y_hat = self(x)
-        loss = F.binary_cross_entropy(y_hat, y.float().unsqueeze(0))
+        loss = self.criterion(y_hat.squeeze(0), y.float())
+        threshold = torch.tensor([0.5]).type_as(y_hat)
+        y_hat = (y_hat>threshold).float()*1
+        self.train_accuracy(y_hat.squeeze(0), y)
         return loss
+    
+    def training_epoch_end(self, outputs):
+        train_loss = torch.tensor([dict["loss"] for dict in outputs]).mean()
+        epoch_acc = self.train_accuracy.compute()
+        self.train_accuracy.reset()
+        self.log('train_loss', train_loss, prog_bar=True)
+        self.log('train_acc', epoch_acc, prog_bar=True)
     
     def validation_step(self, batch, batch_idx):
         # B x T x C x H x W
         x, y = batch
         y_hat = self(x)
-        loss = F.binary_cross_entropy(y_hat, y.float().unsqueeze(0))
-        self.val_accuracy(y_hat, y.unsqueeze(0))
-        self.log('val_loss', loss)
+        loss = self.criterion(y_hat.squeeze(0), y.float())
+        threshold = torch.tensor([0.5]).type_as(y_hat)
+        y_hat = (y_hat>threshold).float()*1
+        self.valid_accuracy(y_hat.squeeze(0), y)
         return loss
     
+    def validation_epoch_end(self, outputs):
+        val_loss = torch.tensor(outputs).mean()
+        epoch_acc = self.valid_accuracy.compute()
+        self.valid_accuracy.reset()
+        self.log('val_loss', val_loss, prog_bar=True)
+        self.log('val_acc', epoch_acc, prog_bar=True)
+    
     def configure_optimizers(self):
-        return torch.optim.SGD(self.parameters(), lr=0.001)
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0001)
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min')
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": lr_scheduler,
+                "monitor": "val_loss",
+            },
+        }
 
 class ViolenceDataset(pl.LightningDataModule):
     def __init__(self, data_dir: str = 'path/to/dir', batch_size: int = 1, num_clips: int = 16):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.num_clips = num_clips
     
     def setup(self, stage: Optional[str] = None):
-        pipeline = torchvision.transforms.Compose([
-        torchvision.transforms.CenterCrop(112),
-        torchvision.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.Normalize((0.43216, 0.394666, 0.37645), (0.22803, 0.22145, 0.216989))])
-
-        violence_full = SpatioTemporalDataset(
-            self.data_dir,
-            transforms=pipeline,
-            num_clips=16)
+        violence_full = SpatioTemporalDataset(self.data_dir, num_clips=self.num_clips)
         self.train, self.val = stratified_random_split(violence_full, (0.7,0.3), violence_full.targets())
+
+        train_pipeline = torchvision.transforms.Compose([
+            torchvision.transforms.CenterCrop(112),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.Normalize((0.43216, 0.394666, 0.37645), (0.22803, 0.22145, 0.216989))])
+        self.train.dataset.transforms = train_pipeline
+        self.train.dataset.train = True
+
+        valid_pipeline = torchvision.transforms.Compose([
+            torchvision.transforms.CenterCrop(112),
+            torchvision.transforms.Normalize((0.43216, 0.394666, 0.37645), (0.22803, 0.22145, 0.216989))])
+        self.val.dataset.transforms = valid_pipeline
+        self.val.dataset.train = False
     
     def train_dataloader(self):
-        return DataLoader(self.train, batch_size=self.batch_size, shuffle=True, num_workers=4)
+        return DataLoader(self.train, batch_size=self.batch_size, shuffle=True, num_workers=8)
     
     def val_dataloader(self):
-        return DataLoader(self.val, batch_size=self.batch_size, shuffle=False, num_workers=4)
+        return DataLoader(self.val, batch_size=self.batch_size, shuffle=False, num_workers=8)
 
 if __name__ == '__main__':
-    data = ViolenceDataset('/mnt/d/serao/fight_detection', batch_size=1)
+    data = ViolenceDataset('/mnt/d/serao/fight_detection', num_clips=8, batch_size=1)
+
     model = ViolenceDetection()
 
-    early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=0.00, patience=3, verbose=False, mode="min")
+    es = EarlyStopping(monitor="val_acc", min_delta=0.001, patience=3, verbose=False, mode="max")
+    lr_monitor = LearningRateMonitor(logging_interval='epoch', log_momentum=True)
 
-    trainer = pl.Trainer(callbacks=[early_stop_callback], gpus=1)
+    trainer = pl.Trainer(callbacks=[es, lr_monitor], gpus=1, max_epochs=60, num_sanity_val_steps=0)
     trainer.fit(model, data)
